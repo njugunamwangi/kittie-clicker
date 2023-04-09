d299539c325d78d56161d9c3c4b89f88b9d16e82
njugunamwangi <njugunamwangi.n@gmail.com>
2023-04-09 22:06:59 +0300

    View.vue files

---
+++ src/views/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main-page p-d-flex p-jc-center p-ai-center">
+    <h1>KitKat</h1>
+    <p>Purrs: 19999</p>
+    <div class="cat-circle"></div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+.cat-circle
+  width: 300px
+  height: 300px
+  border-radius: 50%
+  border: 2px solid black
+  background-color: #efe92e
+</style>
