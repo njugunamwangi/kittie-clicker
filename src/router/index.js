952246ad99f0fc631af97f49ac454096b7faba7d
njugunamwangi <njugunamwangi.n@gmail.com>
2023-04-09 22:05:21 +0300

    refactor router

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import WelcomeView from "../views/WelcomeView.vue";
+import MainView from "../views/MainView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "welcome",
+    component: WelcomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/main",
+    name: "main",
+    component: MainView,
   },
 ];
 
