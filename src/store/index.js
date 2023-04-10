ad4fa62e923168fe90be0fe82f979fb50d1dcd41
njugunamwangi <njugunamwangi.n@gmail.com>
2023-04-10 14:02:06 +0300

    kitten properties

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    kittenName: "",
+    kittenUrl: "",
+    kittens: [
+      "https://placekitten.com/300/300",
+      "https://placekitten.com/300/304",
+      "https://placekitten.com/300/305",
+    ],
+  },
   getters: {},
   mutations: {},
   actions: {},
