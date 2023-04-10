19378624d0a3dda81346009de39f890154a3bed3
njugunamwangi <njugunamwangi.n@gmail.com>
2023-04-10 14:59:10 +0300

    export Toast component throughout the app

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
-  <Toast position="top-right" />
+  <Toast position="top-right" group="tr" />
   <router-view />
 </template>
+
+<script>
+import Toast from "primevue/toast";
+
+export default {
+  components: {
+    Toast,
+  },
+};
+</script>
