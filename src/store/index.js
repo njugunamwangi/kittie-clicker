1ed512660309cb327fabddbe134c8a0034b20588
njugunamwangi <njugunamwangi.n@gmail.com>
2023-04-11 01:45:20 +0300

    add persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
   state: {
@@ -18,4 +19,5 @@ export default createStore({
   },
   actions: {},
   modules: {},
+  plugins: [createPersistedState()],
 });
